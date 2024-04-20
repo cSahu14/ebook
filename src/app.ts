87a19ade240b3c4bcfe563294075a21c7d1af230
cSahu14 <sahuchidananda1999@gmail.com>
2024-04-20 11:02:52 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/golbalErrorHandler";
 
 const app = express();
 
@@ -7,5 +8,9 @@ const app = express();
 app.get("/", (req, res, next) => {
   res.json({ message: "Hello world." });
 });
+
+// Global error handler
+
+app.use(globalErrorHandler);
 
 export default app;
